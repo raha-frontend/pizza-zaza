743b6e8b428d72582fe2de5181f7a554c8f65062
Rahman <52715445+raha-frontend@users.noreply.github.com>
2023-06-22 19:26:34 +0300

    format code

--- src/store/modules/cart/index.ts
+++ src/store/modules/cart/index.ts
@@ -1,19 +1,19 @@
-import { defineStore } from 'pinia'
+import { defineStore } from "pinia";
 
 export const useCartStore = defineStore({
-	id: 'cart',
+  id: "cart",
 
-	state: () => ({
-		cart: []
-	}),
+  state: () => ({
+    cart: [],
+  }),
 
-	actions: {
-		addToCart(car) {
-			this.cart.push(car)
-		},
+  actions: {
+    addToCart(car) {
+      this.cart.push(car);
+    },
 
-		deleteById(carId) {
-			this.cart = this.cart.filter(car => car.id !== carId)
-		} 
-	}
-})
+    deleteById(carId) {
+      this.cart = this.cart.filter((car) => car.id !== carId);
+    },
+  },
+});
