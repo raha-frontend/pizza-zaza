45a58fd857769141061073c11263cfc6c022c04a
Rahman <52715445+raha-frontend@users.noreply.github.com>
2023-07-11 16:08:04 +0300

    fix: Button

--- src/components/UI/AppButton.vue
+++ src/components/UI/AppButton.vue
@@ -1,63 +1,63 @@
 <template>
-	<button
-		:type="type"
-		:disabled="disabled"
-		class="app-button"
-		:class="{ 'app-button_secondory': theme === 'secondory' }"
-	>
-		<slot />
-	</button>
+  <button
+      :type="type"
+      :disabled="disabled"
+      class="app-button"
+      :class="{ 'app-button_secondory': theme === 'secondory' }"
+  >
+    <slot/>
+  </button>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue'
 
 export default defineComponent({
-	props: {
-		theme: {
-			type: String,
-			default: 'primary' // primary, secondory
-		},
-		type: {
-			type: String,
-			default: 'button'
-		},
-		disabled: {
-			type: Boolean,
-			default: false
-		}
-	},
+  props: {
+    theme: {
+      type: String,
+      default: 'primary' // primary, secondory
+    },
+    type: {
+      type: String,
+      default: 'button'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
 
-	setup() {
-		
-	},
+  setup() {
+
+  },
 })
 </script>
 
-<style lang="scss"  scoped>
-	.app-button {
-		padding: 10px 25px;
-		font-weight: 500;
-		font-size: 16px;
-		line-height: 31px;
-		border: none;
-		background-color: var(--color-btn);
-		border-bottom: 1px solid var(--color-black);
-		border-radius: 24px;
-		color: #fff;
-		cursor: pointer;
-		transition: all 0.3s linear;
+<style lang="scss" scoped>
+.app-button {
+  padding: 10px 25px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 31px;
+  border: none;
+  background-color: var(--color-btn);
+  border-bottom: 1px solid var(--color-black);
+  border-radius: 24px;
+  color: #fff;
+  cursor: pointer;
+  transition: all 0.3s linear;
 
-		&:hover {
-			background-color: var(--color-black);
-		}
+  &:hover {
+    background-color: var(--color-black);
+  }
 
-		&_secondory {
-			background-color: var(--color-black);
+  &_secondory {
+    background-color: var(--color-black);
 
-			&:hover {
-				background-color: #333;
-			}
-		}
-	}
+    &:hover {
+      background-color: #333;
+    }
+  }
+}
 </style>
